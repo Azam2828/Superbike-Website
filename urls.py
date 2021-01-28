0c6d506d9529811ea139ac5e7e71930c6f87d27e
MD Azam Khan <azam.saifies@gmail.com>
2021-01-28 08:47:46 -0800

    Update urls.py

--- urls.py
+++ urls.py
@@ -12,8 +12,12 @@ urlpatterns = [
     path("sportbikes",views.sportbikes, name='sportbikes'),
     path("cruiserbikes",views.cruiserbikes, name='cruiserbikes'),
     path("adventure",views.adventure, name='adventure'),
+    path("touring",views.touring, name='touring'),
+    path("scrambler",views.scrambler, name='scrambler'),
+    path("caferacer",views.caferacer, name='caferacer'),
+    path("naked",views.naked, name='naked'),
     path("family",views.family, name='family'),
-    path("bounvile",views.bounvile, name='bounvile'),
+    path("ducatiscrambler",views.ducatiscrambler, name='ducatiscrambler'),
     path("hayabusa",views.hayabusa, name='hayabusa'),
     path("privacy",views.privacy, name='privacy')
     
